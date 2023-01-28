c531c2a00cd16e6565dc6febc3a6475589a0a83d
WE773533700 <123242811+WE773533700@users.noreply.github.com>
2023-01-28 13:10:41 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -39,7 +39,7 @@ from YukkiMusic.utils.stream.stream import stream
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 @app.on_message(
-    command(["قناه","قناة","شغل","تشغيل"])
+    command("شغل","تشغيل","ران شغلي","شغلي"],"")
     & filters.channel
     & ~filters.edited
     & ~BANNED_USERS
